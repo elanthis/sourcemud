3a5a4dd961b78f2fc306b2a2f8d113218b7e7430
Sean Middleditch <elanthis@awesomeplay.com>
2006-09-05 07:45:41 -0400

    Summary: config fixes
    Fix name of awemud.conf parameter for setting the HTML template directory.
    Make the awemud start script work properly again.

--- src/mud/settings.cc
+++ src/mud/settings.cc
@@ -54,7 +54,7 @@ namespace {
 		SETTING_STRING(scripts_path, 0, NULL, "script_dir", "scripts")
 		SETTING_STRING(world_path, 0, NULL, "world_dir", "data")
 		SETTING_STRING(misc_path, 0, NULL, "misc_dir", "data")
-		SETTING_STRING(html_path, 0, NULL, "misc_dir", "data/html")
+		SETTING_STRING(html_path, 0, NULL, "html_dir", "data/html")
 		SETTING_STRING(db_path, 'D', "db", "db", "data/awemud.db")
 		SETTING_STRING(abuse_email, 0, NULL, "abuse_mail", "awemud@localhost")
 		SETTING_STRING(bugs_email, 0, NULL, "bug_mail", "awemud@localhost")
