0e827c71edc9703fbacc7a54b6df58af963ce045
Sean Middleditch <sean@awesomeplay.com>
2008-11-26 18:20:58 -0500

    remove debug from file list

--- src/common/file.cc
+++ src/common/file.cc
@@ -24,32 +24,22 @@ StringList file::getFileList(String path, String ext)
 	// read all entries
 	dirent* d;
 	while ((d = readdir(dir)) != NULL) {
-		Log::Info << path << "/" << d->d_name;
-
 		// skip entries with a leading .
 		if (d->d_name[0] == '.')
 			continue;
-
-		Log::Info << "no dot";
 
 		// length must be long enough for the extension and a .
 		// and an actual name
 		if (strlen(d->d_name) < ext.size() + 2)
 			continue;
 
-		Log::Info << "long enough";
-
 		// must be a . before the extension
 		if (d->d_name[strlen(d->d_name) - ext.size() - 1] != '.')
 			continue;
 
-		Log::Info << "ext dot";
-
 		// and of course must have the actual extension
 		if (ext != &d->d_name[strlen(d->d_name) - ext.size()])
 			continue;
-
-		Log::Info << "ext";
 
 		// add file to our result list!
 		list.push_back(path + S("/") + String(d->d_name));
