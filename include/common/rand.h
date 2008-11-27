13b7dc4320f09b5e6211c1773ae9827a48bc5372
Sean Middleditch <sean@awesomeplay.com>
2008-11-27 03:18:24 -0500

    cleanup

--- include/common/rand.h
+++ include/common/rand.h
@@ -8,8 +8,8 @@
 #ifndef RAND_H
 #define RAND_H
 
-void init_random (void);		// initializes random numbers
-int get_random (int range);		// returns 0 - range
-int roll_dice (int dice, int sides);	// returns <dice>d<sides> roll
+void init_random();		// initializes random numbers
+int get_random(int range);		// returns 0 - range
+int roll_dice(int dice, int sides);	// returns <dice>d<sides> roll
 
 #endif
